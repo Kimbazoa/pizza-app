31214ba4745b5254eba580ff30a4e763def0d0a7
Olivia Kimberly Nseng Mbazoa <noliviakimberly@gmail.com>
2024-11-17 20:56:57 +0100

    initial commit

---
+++ 2024-TP2-Kotlin/app/src/main/java/com/example/androidtp2/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.androidtp2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    public fun registerNewAccount(view: View)
+    {
+        val intent = Intent(this, RegisterActivity::class.java);
+        startActivity(intent);
+    }
+}
